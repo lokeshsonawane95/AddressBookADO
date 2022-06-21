4160c5c37f096f78def00c77b8f943043fa9e970
lokeshsonawane95 <lokeshsonawane95@gmail.com>
2022-06-21 15:55:59 +0530

    Added to main

---
+++ AddressBookADO/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AddressBookADO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
